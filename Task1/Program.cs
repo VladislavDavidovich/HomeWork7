90d95666d111b9ec5a638f1d05e69850295a58d0
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-08-31 23:48:57 +0300

    Сохранение 1

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matr = Fillmatrix(m, n, 10, -10);
+Printmatrix(matr);
+
+void Printmatrix(double[,] matrix)
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+    
+        }
+        Console.WriteLine();
+    }
+}    
+
+double[,] Fillmatrix(int rousCount, int columnsCount, int leftRange, int rightRange)
+{
+    
+    double[,] matrix = new double [leftRange , rightRange];
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+      return matrix;  
+}
+
+
+
+//().NextDouble
+//double[,] Fillmatrix(int rousCount, int columnsCount, int leftRange, int rightRange)
+//leftRange, rightRange
+// for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
